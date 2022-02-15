98985bdd273e8182ea2b347f4f4fb80dafef1f61
IhorMaloyyy <ihormaloyyy@gmail.com>
2022-02-15 14:21:07 +0200

    added tahometer

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,11 +4,14 @@ using TMPro;
 public class PlayerController : MonoBehaviour
 {
     private float speed;
+    private float rpm;
+
     [SerializeField] private float turnSpeed = 100.0f;
     private Rigidbody playerRb;
     [SerializeField] private GameObject centerOfMass;
 
     [SerializeField] private TextMeshProUGUI speedometerText;
+    [SerializeField] private TextMeshProUGUI rpmText;
 
     [SerializeField] private float horsePower;
     private float horizontalInput;
@@ -23,10 +26,16 @@ public class PlayerController : MonoBehaviour
     {
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
+        
         playerRb.AddRelativeForce(Vector3.forward * horsePower * verticalInput);
         /*transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);*/
+        
         transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+        
         speed = Mathf.Round(playerRb.velocity.magnitude * 3.6f);
         speedometerText.SetText($"Speed: {speed} km/h");
+
+        rpm = Mathf.Round((speed % 30) * 40);
+        rpmText.SetText($"RPM: {rpm}");
     }
 }
