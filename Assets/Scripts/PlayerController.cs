f8d8fbf86f5352a8625f4b01de867fce9275001f
IhorMaloyyy <ihormaloyyy@gmail.com>
2022-02-15 13:09:16 +0200

    chanched type of moving car

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,16 +2,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float speed = 15.0f;
+    /*[SerializeField] private float speed = 15.0f;*/
     [SerializeField] private float turnSpeed = 100.0f;
+    private Rigidbody playerRb;
+
+    [SerializeField] private float horsePower;
     private float horizontalInput;
-    private float forwardInput;
-    
+    private float verticalInput;
+
+    private void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
     void FixedUpdate()
     {
         horizontalInput = Input.GetAxis("Horizontal");
-        forwardInput = Input.GetAxis("Vertical");
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        verticalInput = Input.GetAxis("Vertical");
+        playerRb.AddRelativeForce(Vector3.forward * horsePower * verticalInput);
+        /*transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);*/
         transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
